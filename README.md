5b53d3d1ff4fa20f49faedc3fe2b8bb5e2ecdbe2
Francisco Hodge <hello@franciscohodge.com>
2018-10-14 16:04:11 -0400

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# demo_npm_simple-keyboard_angular-js
-Created with CodeSandbox
---
+++ src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component } from "@angular/core";
+import Keyboard from "simple-keyboard";
+import "simple-keyboard/build/css/index.css";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  title = "simple-keyboard";
+  value = "";
+
+  ngOnInit() {
+    this.keyboard = new Keyboard({
+      onChange: input => this.onChange(input),
+      onKeyPress: button => this.onKeyPress(button)
+    });
+  }
+
+  onChange = input => {
+    this.value = input;
+    console.log("Input changed", input);
+  };
+
+  onKeyPress = button => {
+    console.log("Button pressed", button);
+
+    /**
+     * If you want to handle the shift and caps lock buttons
+     */
+    if (button === "{shift}" || button === "{lock}") this.handleShift();
+  };
+
+  onInputChange = event => {
+    this.keyboard.setInput(event.target.value);
+  };
+
+  handleShift = () => {
+    let currentLayout = this.keyboard.options.layoutName;
+    let shiftToggle = currentLayout === "default" ? "shift" : "default";
+
+    this.keyboard.setOptions({
+      layoutName: shiftToggle
+    });
+  };
+}
