79c8af2e9ba00b1f0602fea8741839cd2d2a5dd8
Francisco Hodge <hello@franciscohodge.com>
2018-12-02 14:12:38 -0500

    full-keyboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,99 @@ import "simple-keyboard/build/css/index.css";
 })
 export class AppComponent {
   value = "";
+  commonKeyboardOptions = {
+    onChange: (input: string) => this.onChange(input),
+    onKeyPress: (button: string) => this.onKeyPress(button),
+    theme: "simple-keyboard hg-theme-default hg-layout-default",
+    physicalKeyboardHighlight: true,
+    syncInstanceInputs: true,
+    mergeDisplay: true,
+    debug: true
+  };
   keyboard: Keyboard;
+  keyboardControlPad: Keyboard;
+  keyboardArrows: Keyboard;
+  keyboardNumPad: Keyboard;
+  keyboardNumPadEnd: Keyboard;
 
   ngOnInit() {
-    this.keyboard = new Keyboard({
-      onChange: input => this.onChange(input),
-      onKeyPress: button => this.onKeyPress(button)
+    this.keyboard = new Keyboard(".simple-keyboard-main", {
+      ...this.commonKeyboardOptions,
+      /**
+       * Layout by:
+       * Sterling Butters (https://github.com/SterlingButters)
+       */
+      layout: {
+        default: [
+          "{escape} {f1} {f2} {f3} {f4} {f5} {f6} {f7} {f8} {f9} {f10} {f11} {f12}",
+          "` 1 2 3 4 5 6 7 8 9 0 - = {backspace}",
+          "{tab} q w e r t y u i o p [ ] \\",
+          "{capslock} a s d f g h j k l ; ' {enter}",
+          "{shiftleft} z x c v b n m , . / {shiftright}",
+          "{controlleft} {altleft} {metaleft} {space} {metaright} {altright}"
+        ],
+        shift: [
+          "{escape} {f1} {f2} {f3} {f4} {f5} {f6} {f7} {f8} {f9} {f10} {f11} {f12}",
+          "~ ! @ # $ % ^ & * ( ) _ + {backspace}",
+          "{tab} Q W E R T Y U I O P { } |",
+          '{capslock} A S D F G H J K L : " {enter}',
+          "{shiftleft} Z X C V B N M < > ? {shiftright}",
+          "{controlleft} {altleft} {metaleft} {space} {metaright} {altright}"
+        ]
+      },
+      display: {
+        "{escape}": "esc ⎋",
+        "{tab}": "tab ⇥",
+        "{backspace}": "backspace ⌫",
+        "{enter}": "enter ↵",
+        "{capslock}": "caps lock ⇪",
+        "{shiftleft}": "shift ⇧",
+        "{shiftright}": "shift ⇧",
+        "{controlleft}": "ctrl ⌃",
+        "{controlright}": "ctrl ⌃",
+        "{altleft}": "alt ⌥",
+        "{altright}": "alt ⌥",
+        "{metaleft}": "cmd ⌘",
+        "{metaright}": "cmd ⌘"
+      }
+    });
+
+    this.keyboardControlPad = new Keyboard(".simple-keyboard-control", {
+      ...this.commonKeyboardOptions,
+      layout: {
+        default: [
+          "{prtscr} {scrolllock} {pause}",
+          "{insert} {home} {pageup}",
+          "{delete} {end} {pagedown}"
+        ]
+      }
+    });
+
+    this.keyboardArrows = new Keyboard(".simple-keyboard-arrows", {
+      ...this.commonKeyboardOptions,
+      layout: {
+        default: ["{arrowup}", "{arrowleft} {arrowdown} {arrowright}"]
+      }
+    });
+
+    this.keyboardNumPad = new Keyboard(".simple-keyboard-numpad", {
+      ...this.commonKeyboardOptions,
+      layout: {
+        default: [
+          "{numlock} {numpaddivide} {numpadmultiply}",
+          "{numpad7} {numpad8} {numpad9}",
+          "{numpad4} {numpad5} {numpad6}",
+          "{numpad1} {numpad2} {numpad3}",
+          "{numpad0} {numpaddecimal}"
+        ]
+      }
+    });
+
+    this.keyboardNumPadEnd = new Keyboard(".simple-keyboard-numpadEnd", {
+      ...this.commonKeyboardOptions,
+      layout: {
+        default: ["{numpadsubtract}", "{numpadadd}", "{numpadenter}"]
+      }
     });
   }
 
